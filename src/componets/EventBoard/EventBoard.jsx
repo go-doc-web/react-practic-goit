ae54e41fada5d47a360cac1f01867047c0d2ffbc
Oleg Gubskiy <gubskyolegod@gmail.com>
2023-01-12 12:07:51 +0200

    create component EventBoard-2

--- src/componets/EventBoard/EventBoard.jsx
+++ src/componets/EventBoard/EventBoard.jsx
@@ -1,5 +1,11 @@
 import css from "./EventBoard.module.css";
 
-export const EventBoard = () => {
-  return <div className={css.eventBoard}>Event Cards</div>;
+export const EventBoard = ({ events }) => {
+  return (
+    <div className={css.eventBoard}>
+      {events.map((event) => {
+        return <div key={event.name}>{event.name}</div>;
+      })}
+    </div>
+  );
 };
